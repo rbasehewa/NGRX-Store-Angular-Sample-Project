530329646ac1d7f69d46791d6dc2e7a1920e499c
rbasehewa <rbasehewa@gmail.com>
2021-10-11 15:19:13 +1100

    fix ngrx dippendenices

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerAction } from '../../store/actions';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +12,7 @@ export class RegisterComponent implements OnInit {
 
   form: FormGroup
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private store: Store) { }
 
   ngOnInit(): void {
 
@@ -23,12 +25,14 @@ export class RegisterComponent implements OnInit {
       username:['',Validators.required],
       email:['',Validators.required],
       password:['',Validators.required]
+      
     })
   }
 
   onSubmit(): void{
     //value will give value of the form back
     console.log('submit',this.form.value, this.form.valid)
+    this.store.dispatch(registerAction(this.form.value)) // dispatching the register action to communication status
   }
 
 }
